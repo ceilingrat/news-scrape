2531de03aa23ef5c5c8dbfa0dc31733f1ab6aeaf
ceilingrat <67766223+ceilingrat@users.noreply.github.com>
2022-04-08 03:21:31 +0000

    "run.sh" requests a URL and saves the file to the local filesystem

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "run button pressed"
+# echo "run button pressed"
+
+echo "paste a URL to request:"
+read -p ">  " URL
+
+partialURL=$(echo $URL | grep -o -e "news/.*$" | xargs printf "%s")
+
+curl --create-dirs -o $partialURL -L $URL
